1477b788fd7bf24300f9f72d8a275048c19a3918
jordvaug <jordanvaughn1@gmail.com>
2019-03-14 21:14:57 -0600

    change to nav bar for mobile display

--- client/src/components/appRouter.jsx
+++ client/src/components/appRouter.jsx
@@ -18,44 +18,42 @@ function AppRouter(props) {
     <Router>
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <div className="nav-link">
-                  {auth.isAuthenticated() && <Link to="/home/"> Home</Link>}
-                </div>
-              </li>
-              <li className="nav-item active">
-                <div className="nav-link">
-                  <Link to="/map/"> Map</Link>
-                </div>
-              </li>
-              <li className="nav-item active">
-                <div className="nav-link">
-                  {!auth.isAuthenticated() && <Link to="/login/">Login</Link>}
-                </div>
-              </li>
-              <li className="nav-item active">
-                <div className="nav-link">
-                  <Link to="/signup/">Sign Up</Link>
-                </div>
-              </li>
-              <li className="nav-item active">
-                <div className="nav-link">
-                  {auth.isAuthenticated() && (
-                    <button
-                      className="btn btn-dark"
-                      onClick={() => {
-                        signOut();
-                      }}
-                    >
-                      Sign Out
-                    </button>
-                  )}
-                </div>
-              </li>
-            </ul>
-          </div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <div className="nav-link">
+                {auth.isAuthenticated() && <Link to="/home/"> Home</Link>}
+              </div>
+            </li>
+            <li className="nav-item active">
+              <div className="nav-link">
+                <Link to="/map/"> Map</Link>
+              </div>
+            </li>
+            <li className="nav-item active">
+              <div className="nav-link">
+                {!auth.isAuthenticated() && <Link to="/login/">Login</Link>}
+              </div>
+            </li>
+            <li className="nav-item active">
+              <div className="nav-link">
+                <Link to="/signup/">Sign Up</Link>
+              </div>
+            </li>
+            <li className="nav-item active">
+              <div className="nav-link">
+                {auth.isAuthenticated() && (
+                  <button
+                    className="btn btn-dark"
+                    onClick={() => {
+                      signOut();
+                    }}
+                  >
+                    Sign Out
+                  </button>
+                )}
+              </div>
+            </li>
+          </ul>
           {user.name}
         </nav>
         <Route path="/home/" component={Home} />
